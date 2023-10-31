dbc56210de259b01357fbb0593ca4cd0853c836f
Alec Dong <alec.dong@mail.utoronto.ca>
2023-10-31 10:50:18 -0400

    added user already exists exception handler

--- src/main/java/com/example/cms/controller/exceptions/UserAlreadyExistsException.java
+++ src/main/java/com/example/cms/controller/exceptions/UserAlreadyExistsException.java
@@ -1,2 +1,9 @@
-package com.example.cms.controller.exceptions;public class UserAlreadyExistsException {
+package com.example.cms.controller.exceptions;
+
+public class UserAlreadyExistsException extends RuntimeException{
+
+    public UserAlreadyExistsException() {
+        super("Username already exists. User creation failed.");
+    }
+
 }
